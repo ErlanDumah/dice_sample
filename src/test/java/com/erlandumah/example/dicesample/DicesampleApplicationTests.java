1d5e0d57f21d2e4f757415660f3b2ac3072b03f1
Kinerae <kinerae7@yahoo.com>
2021-09-30 18:25:18 +0200

    Create overall application: use a simple spring setup with database dependency.

---
+++ src/test/java/com/erlandumah/example/dicesample/DicesampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.erlandumah.example.dicesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicesampleApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
